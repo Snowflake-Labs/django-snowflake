343405a47ffac5f7def675b5a7f7c03af81aaa18
Tim Graham <timograham@gmail.com>
2021-10-12 19:16:10 -0400

    Add Django test suite for pull requests

---
+++ .github/workflows/snowflake_settings.py
@@ -0,0 +1,20 @@
+import os
+import uuid
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django_snowflake',
+        'USER': os.environ['SNOWFLAKE_USER'],
+        'PASSWORD': os.environ['SNOWFLAKE_PASSWORD'],
+        'ACCOUNT': os.environ['SNOWFLAKE_ACCOUNT'],
+        'SCHEMA': 'SCHEMA',
+        'WAREHOUSE': os.environ['SNOWFLAKE_WAREHOUSE'],
+        'TEST': {'NAME': 'TEST_DJANGO_' + str(uuid.uuid4()).upper()},
+    },
+}
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+)
+SECRET_KEY = 'django_tests_secret_key'
+USE_TZ = False
