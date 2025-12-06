8a09d172b44694e2922a6c53f83a4e8c21951e79
Tim Graham <timograham@gmail.com>
2025-12-05 20:02:03 -0500

    Update to Django 6.1
    * Snowflake doesn't support database-level delete (#21961)
    * Add support for Bit aggregations

--- django_snowflake/aggregates.py
+++ django_snowflake/aggregates.py
@@ -1,4 +1,16 @@
-from django.db.models.aggregates import StringAgg
+from django.db.models.aggregates import BitAnd, BitOr, BitXor, StringAgg
+
+
+def bit_and(self, compiler, connection, **extra_context):
+    return self.as_sql(compiler, connection, function="BIT_AND_AGG", **extra_context)
+
+
+def bit_or(self, compiler, connection, **extra_context):
+    return self.as_sql(compiler, connection, function="BIT_OR_AGG", **extra_context)
+
+
+def bit_xor(self, compiler, connection, **extra_context):
+    return self.as_sql(compiler, connection, function="BIT_XOR_AGG", **extra_context)
 
 
 def string_agg(self, compiler, connection, **extra_context):
@@ -6,4 +18,7 @@ def string_agg(self, compiler, connection, **extra_context):
 
 
 def register_aggregates():
+    BitAnd.as_snowflake = bit_and
+    BitOr.as_snowflake = bit_or
+    BitXor.as_snowflake = bit_xor
     StringAgg.as_snowflake = string_agg
